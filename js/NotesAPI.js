d1c0f1d079488f7d58656c9f97aac06ecc720709
Ivan Artsiusheuski <i.artsiusheuski@gmail.com>
2021-12-19 10:49:52 +0100

    Start

---
+++ js/NotesAPI.js
@@ -0,0 +1,8 @@
+export default class NotesAPI {
+  static getAllNotes() {
+    const notes = JSON.parse(localStorage.getItem("notesapp-notes") || "[]");
+    return notes;
+  }
+  static saveNote(noteToSave) {}
+  static deleteNote(id) {}
+}
